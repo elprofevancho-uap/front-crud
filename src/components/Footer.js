9bca6ff551d13300b9586e6aba660c68e434757d
IvanLuna <glivanluna@gmail.com>
2026-04-03 12:31:53 -0300

    Mejoras

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logoFoot from '../assets/logo-azul.svg';
+import { ReactComponent as LogoFooter } from '../assets/uap-azul.svg';
 
 const Footer = () => {
   return (
     <footer className="py-5 bg-white border-top mt-auto">
       <div className="container text-center">
-        <img 
-          src={logoFoot} 
-          alt="Logo Footer" 
-          height="60" 
-          className="mb-3" 
-        />
-        <p className="text-muted mb-0">
-          &copy; 2026 - Todos los derechos reservados.
-        </p>
-        <div className="mt-2">
-          <a href="#!" className="text-decoration-none text-muted mx-2">Privacidad</a>
-          <a href="#!" className="text-decoration-none text-muted mx-2">Términos</a>
+        
+        {/* ENVOLTORIO CON TAMAÑO CONTROLADO */}
+       <div style={{ maxWidth: '200px', margin: '0 auto' }} className="mb-3">
+          <LogoFooter className="footer-logo-svg img-fluid" />
         </div>
+
+        <p className="text-muted mb-0">
+          &copy; 2026 - Programacion III - UAP. Todos los derechos reservados.
+        </p>
       </div>
     </footer>
   );
 };
-
 export default Footer;
